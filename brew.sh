53b8a240d8ebfe8ae48782681b498806ac792b90
Troy Williams <maxorsomeness@gmail.com>
2016-01-20 16:03:51 +1300

    Some Brew apps.

---
+++ brew.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# Ask for the administrator password upfront.
+sudo -v
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade --all
+
+# Install GNU core utilities (those that come with OS X are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
+
+# Install some other useful utilities like `sponge`.
+brew install moreutils
+
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+brew install findutils
+
+# Install GNU `sed`, overwriting the built-in `sed`.
+brew install gnu-sed --with-default-names
+
+# Install Bash 4.
+# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
+# running `chsh`.
+brew install bash
+brew tap homebrew/versions
+brew install bash-completion2
+
+# Install cask.
+brew install caskroom/cask/brew-cask
+brew tap caskroom/versions
+
+# Install `wget` with IRI support.
+brew install wget --with-iri
+
+# Install RingoJS and Narwhal.
+# Note that the order in which these are installed is important;
+# see http://git.io/brew-narwhal-ringo.
+brew install ringojs
+brew install narwhal
+
+# Install more recent versions of some OS X tools.
+brew install vim --override-system-vi
+brew install homebrew/dupes/grep
+brew install homebrew/dupes/openssh
+brew install homebrew/dupes/screen
+
+brew install homebrew/php/php56 --with-gmp
+
+# Git.
+brew install git
+brew install git-lfs
+
+# Composer.
+brew install composer
+
+# Network.
+brew install mtr
+
+# Other.
+brew install webkit2png
+brew install imagemagick --with-webp
+brew install ffmpeg --with-libvpx
+
+# Remove outdated versions from the cellar.
+brew cleanup
