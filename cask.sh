03d1b52354a45717309634f87c06ba712819c63f
Troy Williams <maxorsomeness@gmail.com>
2016-01-20 16:05:40 +1300

    Some BrewCask apps.

---
+++ cask.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Ask for the administrator password upfront.
+sudo -v
+
+brew upgrade brew-cask
+
+# Important.
+brew cask install google-drive
+brew cask install virtualbox
+brew cask install virtualbox-extension-pack
+brew cask install vagrant
+brew cask install vagrant-bar
+brew cask install packer
+brew cask install vault
+brew cask install node
+brew cask install mysql-utilities
+brew cask install mysqlworkbench
+
+# Browsers.
+brew cask install google-chrome-canary
+brew cask install firefox-nightly
+
+# Media.
+brew cask install miro-video-converter
+brew cask install vlc
+
+# Other
+brew cask install gpgtools
+
+# cleanup.
+brew cask cleanup
